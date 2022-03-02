be897b8fc034edd03ab5f812215f1fb6125a7e07
Lucas Virgil <virgill@email.unc.edu>
2022-03-02 12:32:48 +0100

    dashboard pt1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   # Not useful for now, but if we add other coins it will
   # has_many :coins, through: :transactions
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :card_info, presence: true
-  validates :card_info, uniqueness: true, length: { minimum: 16 }
-  validates :card_info, numericality: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :card_info, presence: true
+  # validates :card_info, uniqueness: true, length: { minimum: 16 }
+  # validates :card_info, numericality: true
 
 end
