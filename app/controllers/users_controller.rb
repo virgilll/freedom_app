3a09b2fdfc6621331df0fd8fce679d0f1971b98a
Lucas Virgil <virgill@email.unc.edu>
2022-03-04 16:05:32 +0100

    circle edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @transactions = Transaction.where(user: current_user)
     @invested_amount = @transactions.sum(:rounded_amount).round(2)
-    @coin_balance = @transactions.sum(:coin_amount)
+    @coin_balance = @transactions.sum(:coin_amount) #sum coin amnt of transactions
     @dollar_balance = (@coin_balance * Coin.bitcoin_price).round(2)
     @earnings = (@dollar_balance - @invested_amount).round(2)
     #@earnings = Balance of roundup x (current price of btc - avg price of btc)
