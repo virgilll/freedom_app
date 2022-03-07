62adc6debf6619f97cb8a0e6a880150ff738a2c8
Zakaria Adili <adilizakaria1@gmail.com>
2022-03-07 16:09:50 +0100

    FAQ

---
+++ app/javascript/src/help.js
@@ -0,0 +1,9 @@
+import
+<script>
+  const accordion = document.getElementsByClassName ('contentBx');
+for (i = 0; i<accordion.length; i++ ){
+  accordion[i].addEventListener('click', function(){
+    this.classList.toogle('active')
+  })
+}
+</script>
